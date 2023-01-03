5386e603db5560edde82ea1c555c1155f49ecb43
Asma-Alhabsi-as93 <asma93alhabsi@gmail.com>
2023-01-03 04:55:46 -0800

    ubdate the task about hotels

--- Hotel.java
+++ Hotel.java
@@ -1,125 +1,193 @@
 package HotelDBMS;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hotel {
-	public static boolean  HotelTable() {
-		String url = "jdbc:mysql://localhost:3306/HotelDBMS";
-		String username = "root";
-		String password = "root";
-		 Scanner sc = new Scanner(System.in);
-		 String sqlDB = "CREATE TABLE Hotel"
-		         +"(id INTEGER ,"
-				 +" hotel_name VARCHAR(8) not NULL,"
-				 +"hotel_location varchar(20),"
-				+"created_date Date not NULL," 
-				 +"updated_date Date,"
-				+"is_Active  Boolean not NULL,"
-				 +"PRIMARY KEY (id))";
-		 
-		 String insert ="Insert into Hotel values(1,'almaha','muscat','2002-02-20','2003-02-02',true)";
-		 
-		 Connection conn = null;
-			try {
-				Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
-				DriverManager.registerDriver(driver);
-				conn = DriverManager.getConnection(url,username, password);
+	static String url = "jdbc:mysql://localhost:3306/HotelDBMS";
+	static String username = "root";
+	static String password = "root";
+
+	public static boolean HotelTable() {
+
+		Scanner sc = new Scanner(System.in);
+		String sqlDB = "CREATE TABLE Hotel" + "(id INTEGER ," + " hotel_name VARCHAR(8) not NULL,"
+				+ "hotel_location varchar(20)," + "created_date Date not NULL," + "updated_date Date,"
+				+ "is_Active  BIT not NULL," + "PRIMARY KEY (id))";
+
+		Connection conn = null;
+		try {
+			Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
+			DriverManager.registerDriver(driver);
+			conn = DriverManager.getConnection(url, username, password);
+			Statement st = conn.createStatement();
+			int m = st.executeUpdate(sqlDB);
+
+			if ((m >= 0)) {
+				System.out.println("Created table in given database...");
+
+			} else {
+				System.out.println(" table already Created in given database...");
+
+				return true;
+			}
+			conn.close();
+		} catch (Exception ex) {
+			System.err.println(ex);
+		}
+		return false;
+	}
+
+	public static void insertIntoTable() throws Throwable {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" how many users you have to print");
+		int insert = sc.nextInt();
+		String hotel_name = "majaan";
+		String hotel_location = "muscat";
+		Date created_date = new Date(System.currentTimeMillis());
+		Date updated_date = new Date(System.currentTimeMillis());
+		boolean is_Active = true;
+
+		Random rn = new Random();
+		Integer NumberToAdd = rn.nextInt(100);
+		Connection conn = null;
+		try {
+			Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
+			DriverManager.registerDriver(driver);
+			conn = DriverManager.getConnection(url, username, password);
+			for (int i = 1; i <= 100; i++) {
+
+				String insert1 = "Insert into Hotel values(" + i + NumberToAdd + ",'" + hotel_name + "','"
+						+ hotel_location + "','" + created_date + "','" + updated_date + "'," + 1 + ")";
+				System.out.println(insert1);
+
 				Statement st = conn.createStatement();
-				int m = st.executeUpdate(sqlDB);
-				int s = st.executeUpdate(insert);
-				if ((m >= 0)&&(s>=1)){
-					System.out.println("Created table in given database...");
-					System.out.println("inserted Successfully...");
-				} else {
-					System.out.println(" table already Created in given database...");
-					System.out.println("insertion failed...");
-					return true;
-				}
+				st.executeUpdate(insert1);
+				System.out.println(insert1 + " Records Inserted Successfully!");
+
+			}
+		} catch (Exception ex) {
+			System.err.println(ex);
+		} finally {
+			if (conn != null)
 				conn.close();
-			}
-			catch (Exception ex) {
-				System.err.println(ex);
-			}
-			return false;
-}
-	public static void readFromTable(){
+		}
+	}
+
+	public static void getById() {
 		String url = "jdbc:mysql://localhost:3306/HotelDBMS";
-		String username = "root";
-		String password = "root";
-		 Scanner sc = new Scanner(System.in);
-		 System.out.println(" how many users you have to print");
-		 String SQL="SELECT * FROM Hotel ORDER BY id LIMIT 100;";
-			Connection conn = null;
-			try {
-				Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
-				DriverManager.registerDriver(driver);
-				conn = DriverManager.getConnection(url,username, password);
-				Statement st = conn.createStatement();
-				ResultSet m = st.executeQuery(SQL);
-				 while(m.next()){
-			            //Display values
-			            System.out.print("ID: " + m.getInt("id"));
-			            System.out.print(", hotel_name: " + m.getString("hotel_name"));
-			            System.out.print(", hotel_location: " + m.getString("hotel_location"));
-			            System.out.println(", created_date: " + m.getDate("created_date"));
-			            System.out.println(", updated_date: " + m.getDate("updated_date"));
-			            System.out.println(", is_Active: " + m.getBoolean("is_Active"));
-				 }
-				conn.close();
+		String user = "root";
+		String pass = "root";
+		Connection con = null;
+		try {
+			Driver driver = (Driver) Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			// Registering drivers
+			DriverManager.registerDriver(driver);
+			// Reference to connection interface
+			con = DriverManager.getConnection(url, user, pass);
+			// Creating a statement
+			Statement st = con.createStatement();
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Please Enter the id: ");
+			int inputid = scanner.nextInt();
+			int count = 1;
+			String sql = "select * from hotel where id='" + inputid + "'";
+			ResultSet rs = st.executeQuery(sql);
+			while (rs.next() && count <= inputid) {
+				int id = rs.getInt("id");
+
+				String hotelname = rs.getString("hotel_name");
+				String hotellocation = rs.getString("hotel_location");
+				Date createddate = rs.getDate("created_date");
+				Date updateddate = rs.getDate("updated_date");
+				boolean isActive = rs.getBoolean("is_Active");
+				System.out.println(id + " " + hotelname + " " + hotellocation + " " + createddate + " " + updateddate
+						+ " " + isActive);
+				count++;
 			}
-			catch (Exception ex) {
-				System.err.println(ex);
-			}
-		 
-			}
-	
-			
-	
-	public static void main(String[] args) {
+		} catch (Exception ex) {
+			System.err.println(ex);
+		}
+	}
+
+	public static void updateById() {
+		String url = "jdbc:mysql://localhost:3306/HotelDBMS";
+		String user = "root";
+		String pass = "root";
+		Scanner scanner = new Scanner(System.in);
+
+		Connection conn = null;
+		try {
+			Driver driver = (Driver) Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			DriverManager.registerDriver(driver);
+			// Reference to connection interface
+			conn = DriverManager.getConnection(url, user, pass);
+			// Creating a statement
+			Statement st = conn.createStatement();
+
+			System.out.println("Please Enter any id to Update hotel data :");
+			int userinput = scanner.nextInt();
+			System.out.println("Please Enter the new Hotel name:");
+			String hotelname = scanner.next();
+			System.out.println("Please Enter the new Hotel location:");
+			String hotellocatin = scanner.next();
+
+			String sql = "UPDATE hotel SET hotel_name='" + hotelname + "',hotel_location='" + hotellocatin
+					+ "' WHERE id='" + userinput + "'";
+			ResultSet result = st.executeQuery(sql);
+		} catch (Exception ex) {
+			System.err.println(ex);
+
+		}
+
+	}
+
+	public static void main(String[] args) throws Throwable {
 		// TODO Auto-generated method stub
-Scanner sc = new Scanner (System .in);
-boolean hasExit=true;
-do {
-	System.out.println("1:craete hotel table");
-	System.out.println("2:Room_Type");
-	System.out.println("3:room ");
-	System.out.println("4: Guests");
-	System.out.println("5:Employee_Type");
-	System.out.println("6:Employees");
-	int select = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		boolean hasExit = true;
+		do {
+			System.out.println("1:insertIntoTable");
+			System.out.println("2:getById");
+			System.out.println("3: updateById");
+			System.out.println("4:deleteById");
+			System.out.println("5:makeIsActiveFalseById");
+			
+			int select = sc.nextInt();
 
-	switch (select) {
-	case 1:
-		 HotelTable();
-		 readFromTable();
-		break;
-		
-	case 2:
-		Room_Type.room_type();
-		Room_Type.readFromTable();
-		 break;
-	case 3:
-		Rooms.room();
-		break;
-		
-	case 4:
-		Guests.Guests();
-		break;
-		
-	case 5:
-			Employee_Type.Employee_Type();
-		break;
-	case 6:
-		Employees.Employees();
-		break;
+			switch (select) {
+			case 1:
+//		 HotelTable();
+//		 
+				insertIntoTable();
+//				
+				break;
+
+			case 2:
+				getById();
+				break;
+			case 3:
+				 updateById();
+				break;
+
+			case 4:
+				 //updateById(); 
+				break;
+
+			case 5:
+				Employee_Type.Employee_Type();
+				break;
+			
+			}
+		} while (hasExit);
 	}
-}while (hasExit);
-	}
-	
+
 }
-
